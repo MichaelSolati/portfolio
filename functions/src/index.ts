1c3b3692e3defa86ea064840607071029273d55c
Michael Solati <mkslt04@gmail.com>
2020-05-17 17:02:13 -0700

    feat(ssr): overwrite `response.render` to potentially intercept results

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,19 @@
 import * as functions from 'firebase-functions';
-import { Express } from 'express';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const ssr = functions.https.onRequest((request, response) => {
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  const app: Express = require(`${process.cwd()}/dist/portfolio/server/main`).app();
+  const app = require(`${process.cwd()}/dist/portfolio/server/main`).app();
+
+  const _render = response.render;
+  //@ts-ignore
+  response.render = function (view: string, options: object): void {
+    _render.bind(this)(view, options, (err: Error, html: string) => {
+      response.send(html);
+    });
+  };
+
   app(request, response);
 });
