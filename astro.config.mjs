e0945c1d10a46b777bfa22785388227f2f15c20a
Michael Solati <mkslt04@gmail.com>
2023-07-10 10:43:00 -0700

    refactor: abstract layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://michaelsolati.com',
 	integrations: [mdx(), sitemap()],
 });
