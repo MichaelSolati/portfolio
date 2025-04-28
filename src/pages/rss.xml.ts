ce690386aa817a85eae08fb871aebafb6f4175dc
Michael Solati <mkslt04@gmail.com>
2025-04-28 12:32:18 -0700

    fix(rss): add post content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,15 +4,17 @@ import {getCollection} from 'astro:content';
 import {environment} from '../environment';
 
 export async function GET(context: APIContext) {
-  const posts = (await getCollection('blog')).sort(
-    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  );
+  const posts = (await getCollection('blog'))
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .slice(0, 24);
+
   return rss({
     title: environment.name,
     description: environment.description,
     site: context.site as URL,
     items: posts.map(post => ({
       ...post.data,
+      content: post.body,
       link: `/blog/${post.slug}/`,
     })),
   });
