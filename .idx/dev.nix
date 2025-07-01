994c25c402a9ff368bf3744bd339da3fd9ee9b77
mkslt04 <mkslt04@gmail.com>
2025-07-01 16:32:03 +0000

    chore: add FIrebase Studio support

---
+++ .idx/dev.nix
@@ -0,0 +1,25 @@
+{pkgs}: {
+  channel = "stable-25.05";
+  packages = [
+    pkgs.nodejs_22
+    pkgs.zulu
+  ];
+  idx = {
+    workspace = {
+      onCreate = {
+        default.openFiles = [
+          "src/app/page.tsx"
+        ];
+      };
+    };
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
